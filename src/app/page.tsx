05f6c507026d431eaf839a819f0d3c4dce93457f
eddyzera <esilva644@gmail.com>
2023-05-17 11:55:47 -0300

    refactor: change fonts in layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="h-screen bg-zinc-900">
-      <h1 className="text-zinc-50 text-4xl font-bold">Sua cápsula do tempo</h1>
+      <h1 className="text-4xl font-bold text-zinc-50">Sua cápsula do tempo</h1>
     </div>
   )
 }
