5857d5d4b5d218c6cc65b77a79f2fe4828cbf365
eddyzera <esilva644@gmail.com>
2023-05-17 12:24:03 -0300

    refactor: use font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={roboto.variable}>{children}</body>
+      <body className={`${roboto.variable} ${baiJamjuree.variable} font-sans`}>{children}</body>
     </html>
   )
 }
